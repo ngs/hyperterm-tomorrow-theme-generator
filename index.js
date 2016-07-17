16e582592652a9e0d1e641f9764d8cf414776f95
Atsushi Nagase <a@ngs.io>
2016-07-17 18:36:48 +0900

    Update metadata

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs =  require('fs-extra')
   , encoding = 'utf-8'
   , indexJSTemplate = ejs.compile(fs.readFileSync(path.join(__dirname, 'templates', 'index.js.ejs'), { encoding }))
   , readMeTemplate = ejs.compile(fs.readFileSync(path.join(__dirname, 'templates', 'Readme.md.ejs'), { encoding }))
+  , basePackageName = fs.readJsonSync(path.join(__dirname, 'package.json')).name
   , colorKeys = [
       'background',
       'red',
@@ -26,20 +27,19 @@ const fs =  require('fs-extra')
     ]
   ;
 
-function buildPackageJSON(vars, json, basePackageJSON) {
-  basePackageJSON = basePackageJSON || JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), { encoding }))
-  json = json || basePackageJSON
-  delete basePackageJSON.scripts;
-  delete basePackageJSON.private;
-  delete basePackageJSON.dependencies;
+function buildPackageJSON(vars, json) {
+  json = json || fs.readJsonSync(path.join(__dirname, 'package.json'))
+  delete json.scripts;
+  delete json.private;
+  delete json.dependencies;
   for(var k in json) {
     var v = json[k];
     switch (typeof v) {
     case 'object':
-      json[k] = buildPackageJSON(vars, v, basePackageJSON);
+      json[k] = buildPackageJSON(vars, v);
       break;
     case 'string':
-      json[k] = v.replace(basePackageJSON.name, vars.theme.packageName);
+      json[k] = v.replace(basePackageName, vars.theme.packageName);
       break;
     default:
       json[k] = v;
@@ -76,6 +76,7 @@ fs.readdir(path.join(__dirname, './themes/'), (err, files) => {
         , packageJSON = buildPackageJSON(templateVars)
         , packageDir = path.join(__dirname, 'dist', theme.packageName)
         ;
+      packageJSON.description = `Hyperterm theme based on ${theme.name}`
       fs.ensureDirSync(packageDir);
       fs.copySync(path.join(__dirname, 'screenshots', `${theme.name}.png`), path.join(packageDir, 'screenshot.png'));
       fs.writeFileSync(path.join(packageDir, 'index.js'), indexJS, { encoding });
